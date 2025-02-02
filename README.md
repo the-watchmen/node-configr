e7d69513ae9fccc7d9ff056cacdc86436bdf35c8
tony kerz <anthony.kerz@gmail.com>
2025-02-02 17:24:58 -0500

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# node-configr
+
+config helper
+
+[![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/sindresorhus/xo)
+[![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+[![build status](https://github.com/the-watchmen/node-helpr/actions/workflows/release.yaml/badge.svg)](https://github.com/the-watchmen/node-configr/actions)
+[![npm (scoped)](https://img.shields.io/npm/v/@watchmen/helpr.svg)](https://www.npmjs.com/package/@watchmen/configr)
+
+> see [tests](test) for examples
+
+## usage
+
+* `npm i @watchmen/configr`
+* `import {...} from '@watchmen/configr'`
+
+## development
+
+1. `git clone {repo name}`
+1. `cd {repo name}`
+1. `npm i`
+1. `npm test`
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import _assert from 'node:assert'
+import fs from 'node:fs'
+import _ from 'lodash'
+import config from 'config'
+import {parse} from 'yaml'
+import debug from '@watchmen/debug'
+import {pretty} from '@watchmen/helpr'
+
+export {getConfig, initConfig}
+
+const dbg = debug(import.meta.url)
+
+const _config = initConfig()
+
+function initConfig() {
+  const yaml = process.env.CONFIGR_YAML
+  if (yaml) {
+    dbg('yaml=%o', yaml)
+    const _yaml = fs.readFileSync(yaml, 'utf8')
+    const __yaml = parse(_yaml)
+    dbg('read yaml=%s', pretty(__yaml))
+    return _.merge(config, __yaml)
+  }
+
+  return config
+}
+
+function getConfig({path, dflt = null}) {
+  const toks = path.split('.')
+  const env = _.snakeCase(toks.join('_')).toUpperCase()
+  const _path = toks.join('.')
+  dbg('env=%s, path=%s, dflt=%s', env, _path, dflt)
+  return process.env[env] || _.get(_config, _path) || dflt
+}
